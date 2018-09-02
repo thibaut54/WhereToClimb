5f45ff1f0c3d241d180160dc3b3498ce4113b4f8
thibaut54 <john_milton100@hotmail.com>
2018-09-03 01:13:51 +0200

    feat<consumer>: implement atlasRepository class and test it in DbSeeder

---
+++ consumer/src/main/java/org/thibaut/wheretoclimb/consumer/repository/AtlasRepository.java
@@ -0,0 +1,9 @@
+package org.thibaut.wheretoclimb.consumer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.thibaut.wheretoclimb.model.bean.Atlas;
+
+@Repository
+public interface AtlasRepository  extends JpaRepository< Atlas, Integer > {
+}
